1d6324aa83a93c8cd3d22c44efdafe4a6732f8b0
dinelly43 <valdirdinelly@hotmail.comgit config --global user.namedinelly43git config --global user.email valdirdinelly@hotmail.com>
2024-02-06 12:06:23 -0300

    inicio do pagode

---
+++ js/index.js
@@ -0,0 +1,11 @@
+mes.onchange = () => {
+    switch(mes.value.toLowerCase()){
+        case "janeiro":
+            nome.innerHTML = "Rei"
+        break;
+        case "fevereiro":
+            nome.innerHTML = "Mô"
+        break;
+    }
+        
+}
