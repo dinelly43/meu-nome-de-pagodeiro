9ea3eca8f4f14a797fdf7b8081946f5f061cd741
dinelly43 <valdirdinelly@hotmail.comgit config --global user.namedinelly43git config --global user.email valdirdinelly@hotmail.com>
2024-02-08 12:16:06 -0300

    dia feito

--- js/index.js
+++ js/index.js
@@ -6,6 +6,44 @@ mes.onchange = () => {
         case "fevereiro":
             nome.innerHTML = "Mô"
         break;
+        case "março":
+            nome.innerHTML = "Cupido"
+        break;
+        case "abril":
+            nome.innerHTML = "Apaixonado"
+        break;
+        case "maio":
+            nome.innerHTML = "Pagodeiro"
+        break;
+        case "junho":
+            nome.innerHTML = "Mensageiro"
+        break;
+        case "julho":
+            nome.innerHTML = "Cara perfeito"
+        break;
+        case "agosto":
+            nome.innerHTML = "Perturbado"
+        break;
+        case "setembro":
+            nome.innerHTML = "Gueri Gueri"
+        break;
+        case "outubro":
+            nome.innerHTML = "Coração"
+        break;
+        case "novembro":
+            nome.innerHTML = "Linda Voz"
+        break;
+        case "dezembro":
+            nome.innerHTML = "Criado Mudo"
+        break;
+        default:
+            nome.innerHTML = "Escolha um mês válido"
     }
-        
 }
+
+dia.onchange = () => {
+    if(dia.value >= 1 && dia.value <=3){
+        nome.innerHTML += "calabreso"
+    }
+}
+
